adf8eb45ae1357c4589f390c4befa1d81d5c2d07
Deborah Nakakande <d.nakakande@hotmail.com>
2020-05-19 13:46:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  [element, ...array]
-  return array
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
@@ -11,8 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element){
-  [...array, element]
-  return array
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
