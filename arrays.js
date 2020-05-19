81cce043262cd721396a7db47e41255ba06c5053
Deborah Nakakande <d.nakakande@hotmail.com>
2020-05-19 13:31:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-
+  [element, ...array]
+  return array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  
+  array.unshift(element)
+  return array
 }
